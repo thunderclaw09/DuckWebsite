eddf9b23db5ccea23ab3c4e0b69769dafd87b87b
thunderclaw09 <elisalam07@gmail.com>
2021-11-18 17:49:00 -0500

    My first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var duckColor = window.prompt("What color of duck would you want to be?")
+document.write("<h2 style='color:black'> You are a "+duckColor+" duck! </h2><br/>");
+
+myarray = [23, 23.23, "testing once more", true];
+// document.write(myarray[2]);
+// var header = document.getElementById("1");
+// header.innerHTML = "testing. You should see this."
+
